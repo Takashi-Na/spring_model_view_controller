78f5c413e2caba08274b92b4d041e3760abd8d2d
Takashi-Na <69133170+Takashi-Na@users.noreply.github.com>
2021-05-29 14:33:20 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_model_view_controller/SpringModelViewControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_model_view_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModelViewControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModelViewControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_model_view_controller/SpringModelViewControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_model_view_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModelViewControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
