b0548d30a0ed27c94c88872e289b750b4848cbe4
Takashi-Na <69133170+Takashi-Na@users.noreply.github.com>
2021-05-29 17:02:22 +0900

    controller実装

---
+++ src/main/java/com/example/spring_model_view_controller/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.spring_model_view_controller.controller;
+
+import com.example.spring_model_view_controller.model.User;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+  
+  @GetMapping("/form")
+  private String readForm(@ModelAttribute User user) {
+    return "form";
+  }
+
+  @PostMapping("/form")
+  private String confirm(@ModelAttribute User user) {
+    return "confirm";
+  }
+}
